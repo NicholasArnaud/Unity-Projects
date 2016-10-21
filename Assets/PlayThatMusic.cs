5c2fce68a15a8132b4bf3f0fb1d3595d4f4127a5
NicholasArnaud <cobrahead09@gmail.com>
2016-10-21 17:02:03 -0500

    Added new Tutorial game Procedural Cave Generation

---
+++ Assets/PlayThatMusic.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayThatMusic : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<TankMovement>() != null)
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
